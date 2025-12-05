d2239f380fee7e4a1b2f611e517f3b2c476fbe0d
Olha Diakiv <olya.dyakiv26@gmail.com>
2025-12-05 09:24:49 +0100

    continue ultimate-react-course/03-pizza-menu

--- ultimate-react-course-main/03-pizza-menu/src/App.jsx
+++ ultimate-react-course-main/03-pizza-menu/src/App.jsx
@@ -1,30 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const pizzaData = [
+    {
+        name: 'Focaccia',
+        ingredients: 'Bread with italian olive oil and rosemary',
+        price: 6,
+        photoName: 'pizzas/focaccia.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Margherita',
+        ingredients: 'Tomato and mozarella',
+        price: 10,
+        photoName: 'pizzas/margherita.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Spinaci',
+        ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+        price: 12,
+        photoName: 'pizzas/spinaci.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Funghi',
+        ingredients: 'Tomato, mozarella, mushrooms, and onion',
+        price: 12,
+        photoName: 'pizzas/funghi.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Salamino',
+        ingredients: 'Tomato, mozarella, and pepperoni',
+        price: 15,
+        photoName: 'pizzas/salamino.jpg',
+        soldOut: true,
+    },
+    {
+        name: 'Pizza Prosciutto',
+        ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+        price: 18,
+        photoName: 'pizzas/prosciutto.jpg',
+        soldOut: false,
+    },
+]
+
+import './index.css'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        <>
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    )
+}
+
+function Header() {
+    // const style = { color: 'red', fontSize: '48px', textTransform: 'uppercase' }
+    return (
+        <header className="header">
+            <h1 className="header">Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu() {
+    const pizzas = pizzaData
+    // const pizzas = []
+    const numPizzas = pizzas.length
+    return (
+        <main className="menu">
+            <h2>Our menu</h2>
+            {numPizzas > 0 ? (
+                <>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, illo!</p>
+                    <ul className="pizzas">
+                        {pizzas.map(pizza => {
+                            return <Pizza key={pizza.name} pizzaObj={pizza} />
+                        })}
+                    </ul>
+                </>
+            ) : (
+                <p>We're still working on our menu</p>
+            )}
+        </main>
+    )
+}
+
+function Pizza({ pizzaObj }) {
+    // if (pizzaObj.soldOut) return null
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
             </div>
-            <h1>Hello React!</h1>
-            <div className="card">
-                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        </li>
+    )
+}
+
+function Footer() {
+    const hour = new Date().getHours()
+    const openHour = 8
+    const closeHour = 21
+    const isOpen = hour >= openHour && hour <= closeHour
+
+    // if (hour >= openHour && hour <= closeHour) {
+    //     alert("We're currently open")
+    // } else {
+    //     alert('Sorry, we closed!')
+    // }
+
+    // if (!isOpen) return <p>We're happy to see you in {openHour}:00</p>
+
+    return <footer>{isOpen ? <Order closeHour={closeHour} /> : <p>We're happy to see you in {openHour}:00</p>}</footer>
+}
+
+function Order({ closeHour }) {
+    return (
+        <div className="order">
+            <p>We're open until {closeHour}:00. Come visit us or order online</p>
+            <button className="btn">Order</button>
+        </div>
     )
 }
 
