acbe2dfab50b7e68c2b723096dcb795fd2027a89
Olha Diakiv <olya.dyakiv26@gmail.com>
2025-12-08 08:51:51 +0100

    add coding train-challanges/04 Purple rain

---
+++ coding-train-challenges/004/drop.js
@@ -0,0 +1,27 @@
+class Drop {
+    constructor() {
+        this.x = random(width)
+        this.y = random(-200, -100)
+        this.z = random(0, 20)
+        this.yspeed = map(this.z, 0, 20, 4, 10)
+        this.length = map(this.z, 0, 20, 10, 20)
+    }
+
+    fall() {
+        this.y = this.y + this.yspeed
+        let grav = map(this.z, 0, 20, 0, 0.2)
+        this.yspeed = this.yspeed + grav
+
+        if (this.y > height) {
+            this.y = random(-200, -100)
+            this.yspeed = map(this.z, 0, 20, 4, 10)
+        }
+    }
+
+    show() {
+        let thick = map(this.z, 0, 20, 1, 3)
+        strokeWeight(thick)
+        stroke(138, 43, 226)
+        line(this.x, this.y, this.x, this.y + this.length)
+    }
+}
