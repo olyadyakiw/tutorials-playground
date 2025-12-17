d11a7996ecee14dc60c035111ef14a44efef19d0
Olha Diakiv <olya.dyakiv26@gmail.com>
2025-12-17 14:08:48 +0100

    continue ultimate-react-course/07-use-popcorn star-rating

--- ultimate-react-course-main/07-use-popcorn/src/App.jsx
+++ ultimate-react-course-main/07-use-popcorn/src/App.jsx
@@ -75,16 +75,8 @@ function NumResults({ movies }) {
     )
 }
 
-function Navbar({ movies }) {
-    return (
-        <nav className="nav-bar">
-            <Logo />
-
-            <Search />
-
-            <NumResults movies={movies} />
-        </nav>
-    )
+function Navbar({ children }) {
+    return <nav className="nav-bar">{children}</nav>
 }
 
 function Movie({ movie }) {
@@ -112,15 +104,15 @@ function MovieList({ movies }) {
     )
 }
 
-function ListBox({ movies }) {
-    const [isOpen1, setIsOpen1] = useState(true)
+function Box({ children }) {
+    const [isOpen, setIsOpen] = useState(true)
 
     return (
         <div className="box">
-            <button className="btn-toggle" onClick={() => setIsOpen1(open => !open)}>
-                {isOpen1 ? '–' : '+'}
+            <button className="btn-toggle" onClick={() => setIsOpen(open => !open)}>
+                {isOpen ? '–' : '+'}
             </button>
-            {isOpen1 && <MovieList movies={movies} />}
+            {isOpen && children}
         </div>
     )
 }
@@ -187,40 +179,30 @@ function WatchedMovieList({ watched }) {
     )
 }
 
-function WatchBox() {
-    const [watched, setWatched] = useState(tempWatchedData)
-    const [isOpen2, setIsOpen2] = useState(true)
-
-    return (
-        <div className="box">
-            <button className="btn-toggle" onClick={() => setIsOpen2(open => !open)}>
-                {isOpen2 ? '–' : '+'}
-            </button>
-            {isOpen2 && (
-                <>
-                    <WatchedSummary watched={watched} />
-                    <WatchedMovieList watched={watched} />
-                </>
-            )}
-        </div>
-    )
-}
-
-function Main({ movies }) {
-    return (
-        <main className="main">
-            <ListBox movies={movies} />
-            <WatchBox />
-        </main>
-    )
+function Main({ children }) {
+    return <main className="main">{children}</main>
 }
 
 export default function App() {
     const [movies, setMovies] = useState(tempMovieData)
+    const [watched, setWatched] = useState(tempWatchedData)
+
     return (
         <>
-            <Navbar movies={movies} />
-            <Main movies={movies} />
+            <Navbar>
+                <Logo />
+                <Search />
+                <NumResults movies={movies} />
+            </Navbar>
+            <Main>
+                <Box>
+                    <MovieList movies={movies} />
+                </Box>
+                <Box>
+                    <WatchedSummary watched={watched} />
+                    <WatchedMovieList watched={watched} />
+                </Box>
+            </Main>
         </>
     )
 }
--- ultimate-react-course-main/07-use-popcorn/src/main.jsx
+++ ultimate-react-course-main/07-use-popcorn/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
+import StarRating from './StarRating.jsx'
+
+function Test() {
+    const [movie, setMovie] = useState()
+    return (
+        <div>
+            <StarRating color="blue" maxRating={10} onSetRating={setMovie} />
+            <p>This movies was rated {movie} stars</p>
+        </div>
+    )
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        {/* <App /> */}
+        <StarRating messages={['Terrible', 'Bad', 'Okay', 'Good', 'Perfect']} />
+        <StarRating className="stars" size={24} color="red" defaultRating={3} />
+
+        <Test />
+    </StrictMode>
 )
