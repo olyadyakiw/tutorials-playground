b380ed831f78d4f887273aac07c2805537f84dbb
Olha Diakiv <olya.dyakiv26@gmail.com>
2025-12-23 09:42:13 +0100

    continue ultimate-react-course/07-use-popcorn

--- ultimate-react-course-main/07-use-popcorn/src/App-v02.jsx
+++ ultimate-react-course-main/07-use-popcorn/src/App-v02.jsx
@@ -2,6 +2,48 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import StarRating from './StarRating'
 
+const tempMovieData = [
+    {
+        imdbID: 'tt1375666',
+        Title: 'Inception',
+        Year: '2010',
+        Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+    },
+    {
+        imdbID: 'tt0133093',
+        Title: 'The Matrix',
+        Year: '1999',
+        Poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+    },
+    {
+        imdbID: 'tt6751668',
+        Title: 'Parasite',
+        Year: '2019',
+        Poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
+    },
+]
+
+const tempWatchedData = [
+    {
+        imdbID: 'tt1375666',
+        Title: 'Inception',
+        Year: '2010',
+        Poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+        runtime: 148,
+        imdbRating: 8.8,
+        userRating: 10,
+    },
+    {
+        imdbID: 'tt0088763',
+        Title: 'Back to the Future',
+        Year: '1985',
+        Poster: 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+        runtime: 116,
+        imdbRating: 8.5,
+        userRating: 9,
+    },
+]
+
 const average = arr => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
 function Logo() {
@@ -73,22 +115,6 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         Genre: genre,
     } = movie
 
-    // if (imdbRating > 8) [isTop, setIsTop] = useState(true)
-    // if (imdbRating > 8) return <p>Great!!</p>
-    // const [isTop, setIsTop] = useState(imdbRating > 8)
-    // console.log(isTop)
-    // useEffect(
-    //     function () {
-    //         setIsTop(imdbRating > 8)
-    //     },
-    //     [imdbRating]
-    // )
-
-    // const isTop = imdbRating > 8
-    // console.log(isTop)
-
-    // const [avgRating, setAvgRating] = useState(0)
-
     useEffect(
         function () {
             async function getMovieDetails() {
@@ -130,8 +156,6 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         }
         onAddWatched(newWatchedMovie)
         onCloseMovie()
-        // setAvgRating(Number(imdbRating))
-        // setAvgRating(avgRating => (avgRating + userRating) / 2)
     }
 
     useEffect(
@@ -169,7 +193,6 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
                             </p>
                         </div>
                     </header>
-                    {/* <p>{avgRating}</p> */}
                     <section>
                         <div className="rating">
                             {!isWatched ? (
@@ -306,10 +329,7 @@ function ErrorMessage({ message }) {
 
 export default function App() {
     const [movies, setMovies] = useState([])
-    const [watched, setWatched] = useState(function () {
-        const stored = localStorage.getItem('watched')
-        return stored ? JSON.parse(stored) : []
-    })
+    const [watched, setWatched] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState('')
     const [query, setQuery] = useState('')
@@ -367,12 +387,11 @@ export default function App() {
         setWatched(watched => watched.filter(movie => movie.imdbID !== id))
     }
 
-    useEffect(
-        function () {
-            localStorage.setItem('watched', JSON.stringify(watched))
-        },
-        [watched]
-    )
+    // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data.search))
+
+    // setWatched([])
 
     return (
         <>
@@ -382,6 +401,7 @@ export default function App() {
                 <NumResults movies={movies} />
             </Navbar>
             <Main>
+                {/* <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box> */}
                 <Box>
                     {isLoading && <Loader />}
                     {!isLoading && !error && <MovieList onSelectdMovie={handleSelectMovie} movies={movies} />}
