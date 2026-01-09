5643c9f28f3ab6b4ffb2ff059b823cb1d77ca51a
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-01-09 17:39:05 +0100

    continue ultimate-react-course/12-atomic-blog

--- ultimate-react-course-main/12-atomic-blog/src/PostContext.js
+++ ultimate-react-course-main/12-atomic-blog/src/PostContext.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useMemo, useState } from 'react'
 
 function createRandomPost() {
     return {
@@ -29,19 +29,17 @@ function PostProvider({ children }) {
         setPosts([])
     }
 
-    return (
-        <PostContext.Provider
-            value={{
-                posts: searchedPosts,
-                onAddPost: handleAddPost,
-                onClearPosts: handleClearPosts,
-                searchQuery,
-                setSearchQuery,
-            }}
-        >
-            {children}
-        </PostContext.Provider>
-    )
+    const value = useMemo(() => {
+        return {
+            posts: searchedPosts,
+            onAddPost: handleAddPost,
+            onClearPosts: handleClearPosts,
+            searchQuery,
+            setSearchQuery,
+        }
+    }, [searchQuery, searchedPosts])
+
+    return <PostContext.Provider value={value}>{children}</PostContext.Provider>
 }
 
 function usePosts() {
