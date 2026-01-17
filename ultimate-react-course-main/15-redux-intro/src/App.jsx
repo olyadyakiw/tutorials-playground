cf371579c276b0c55b9c8d6f660b7f1dbfb4cbe8
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-01-17 17:52:06 +0100

    start ultimate-react-course/15-redux-intro

---
+++ ultimate-react-course-main/15-redux-intro/src/App.jsx
@@ -0,0 +1,18 @@
+import CreateCustomer from './features/customers/CreateCustomer'
+import Customer from './features/customers/Customer'
+import AccountOperations from './features/accounts/AccountOperations'
+import BalanceDisplay from './features/accounts/BalanceDisplay'
+
+function App() {
+    return (
+        <div>
+            <h1>🏦 The React-Redux Bank ⚛️</h1>
+            <CreateCustomer />
+            <Customer />
+            <AccountOperations />
+            <BalanceDisplay />
+        </div>
+    )
+}
+
+export default App
