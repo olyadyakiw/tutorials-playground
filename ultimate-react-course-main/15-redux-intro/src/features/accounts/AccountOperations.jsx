7244dd0667b3224e16b41167fbed794bd16ed588
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-01-18 11:19:35 +0100

    continue ultimate-react-course/15-redux-intro

--- ultimate-react-course-main/15-redux-intro/src/features/accounts/AccountOperations.jsx
+++ ultimate-react-course-main/15-redux-intro/src/features/accounts/AccountOperations.jsx
@@ -1,78 +1,98 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { deposit, paylLoan, requestLoan, withdraw } from './accountSlice'
 
 function AccountOperations() {
-  const [depositAmount, setDepositAmount] = useState("");
-  const [withdrawalAmount, setWithdrawalAmount] = useState("");
-  const [loanAmount, setLoanAmount] = useState("");
-  const [loanPurpose, setLoanPurpose] = useState("");
-  const [currency, setCurrency] = useState("USD");
+    const [depositAmount, setDepositAmount] = useState('')
+    const [withdrawalAmount, setWithdrawalAmount] = useState('')
+    const [loanAmount, setLoanAmount] = useState('')
+    const [loanPurpose, setLoanPurpose] = useState('')
+    const [currency, setCurrency] = useState('USD')
 
-  function handleDeposit() {}
+    const dispatch = useDispatch()
+    const { loan: currentLoan, loanPurpose: currentLoanPurpose, isLoaing } = useSelector(store => store.account)
 
-  function handleWithdrawal() {}
+    function handleDeposit() {
+        if (!depositAmount) return
 
-  function handleRequestLoan() {}
+        dispatch(deposit(depositAmount, currency))
+        setDepositAmount(0)
+        setCurrency('USD')
+    }
 
-  function handlePayLoan() {}
+    function handleWithdrawal() {
+        if (!withdrawalAmount) return
 
-  return (
-    <div>
-      <h2>Your account operations</h2>
-      <div className="inputs">
+        dispatch(withdraw(withdrawalAmount))
+        setWithdrawalAmount(0)
+    }
+
+    function handleRequestLoan() {
+        if (!loanAmount || !loanPurpose) return
+        dispatch(requestLoan(loanAmount, loanPurpose))
+        setLoanAmount(0)
+        setLoanPurpose('')
+    }
+
+    function handlePayLoan() {
+        dispatch(paylLoan())
+    }
+
+    return (
         <div>
-          <label>Deposit</label>
-          <input
-            type="number"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(+e.target.value)}
-          />
-          <select
-            value={currency}
-            onChange={(e) => setCurrency(e.target.value)}
-          >
-            <option value="USD">US Dollar</option>
-            <option value="EUR">Euro</option>
-            <option value="GBP">British Pound</option>
-          </select>
+            <h2>Your account operations</h2>
+            <div className="inputs">
+                <div>
+                    <label>Deposit</label>
+                    <input type="number" value={depositAmount} onChange={e => setDepositAmount(+e.target.value)} />
+                    <select value={currency} onChange={e => setCurrency(e.target.value)}>
+                        <option value="USD">US Dollar</option>
+                        <option value="EUR">Euro</option>
+                        <option value="GBP">British Pound</option>
+                    </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
-        </div>
+                    <button onClick={handleDeposit} disabled={isLoaing}>
+                        {isLoaing ? 'Converting...' : `Deposit ${depositAmount}`}
+                    </button>
+                </div>
 
-        <div>
-          <label>Withdraw</label>
-          <input
-            type="number"
-            value={withdrawalAmount}
-            onChange={(e) => setWithdrawalAmount(+e.target.value)}
-          />
-          <button onClick={handleWithdrawal}>
-            Withdraw {withdrawalAmount}
-          </button>
-        </div>
+                <div>
+                    <label>Withdraw</label>
+                    <input
+                        type="number"
+                        value={withdrawalAmount}
+                        onChange={e => setWithdrawalAmount(+e.target.value)}
+                    />
+                    <button onClick={handleWithdrawal}>Withdraw {withdrawalAmount}</button>
+                </div>
 
-        <div>
-          <label>Request loan</label>
-          <input
-            type="number"
-            value={loanAmount}
-            onChange={(e) => setLoanAmount(+e.target.value)}
-            placeholder="Loan amount"
-          />
-          <input
-            value={loanPurpose}
-            onChange={(e) => setLoanPurpose(e.target.value)}
-            placeholder="Loan purpose"
-          />
-          <button onClick={handleRequestLoan}>Request loan</button>
-        </div>
+                <div>
+                    <label>Request loan</label>
+                    <input
+                        type="number"
+                        value={loanAmount}
+                        onChange={e => setLoanAmount(+e.target.value)}
+                        placeholder="Loan amount"
+                    />
+                    <input
+                        value={loanPurpose}
+                        onChange={e => setLoanPurpose(e.target.value)}
+                        placeholder="Loan purpose"
+                    />
+                    <button onClick={handleRequestLoan}>Request loan</button>
+                </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
+                {currentLoan > 0 && (
+                    <div>
+                        <span>
+                            Pay back ${currentLoan}({currentLoanPurpose})
+                        </span>
+                        <button onClick={handlePayLoan}>Pay loan</button>
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default AccountOperations;
+export default AccountOperations
--- ultimate-react-course-main/15-redux-intro/src/main.jsx
+++ ultimate-react-course-main/15-redux-intro/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
 
 import store from './store.jsx'
 
-store.dispatch({ type: 'account/deposit', payload: '500' })
-
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </StrictMode>
 )
