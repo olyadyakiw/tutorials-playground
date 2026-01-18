b8f6c4342f73ab9a8259210813d7aaf57740c783
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-01-18 15:22:17 +0100

    finish ultimate-react-course/15-redux-intro

--- ultimate-react-course-main/15-redux-intro/src/features/accounts/AccountOperations.jsx
+++ ultimate-react-course-main/15-redux-intro/src/features/accounts/AccountOperations.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { deposit, paylLoan, requestLoan, withdraw } from './accountSlice'
+import { deposit, payLoan, requestLoan, withdraw } from './accountSlice'
 
 function AccountOperations() {
     const [depositAmount, setDepositAmount] = useState('')
@@ -35,7 +35,7 @@ function AccountOperations() {
     }
 
     function handlePayLoan() {
-        dispatch(paylLoan())
+        dispatch(payLoan())
     }
 
     return (
--- ultimate-react-course-main/15-redux-intro/src/features/customers/customerSlice.jsx
+++ ultimate-react-course-main/15-redux-intro/src/features/customers/customerSlice.jsx
@@ -1,29 +1,55 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
     fullName: '',
     initialId: '',
     createdAt: '',
 }
 
-export default function customerReducer(state = initialStateCustomer, action) {
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return {
-                ...state,
-                fullName: action.payload.fullName,
-                nationalId: action.payload.nationalId,
-                createdAt: action.payload.createdAt,
-            }
-        case 'customer/updateName':
-            return { ...state, fullName: action.payload }
-        default:
-            return state
-    }
-}
+const customerSlice = createSlice({
+    name: 'customer',
+    initialState,
+    reducers: {
+        createCustomer: {
+            prepare(fullName, initialId) {
+                return { payload: { fullName, initialId, createdAt: new Date().toISOString() } }
+            },
+            reducer(state, action) {
+                state.fullName = action.payload.fullName
+                state.initialId = action.payload.initialId
+                state.createdAt = action.payload.createdAt
+            },
+        },
+        updateName(state, action) {
+            state.fullName = action.payload
+        },
+    },
+})
 
-export function createCustomer(fullName, nationalId) {
-    return { type: 'customer/createCustomer', payload: { fullName, nationalId, createdAt: new Date().toISOString() } }
-}
+export const { createCustomer, updateName } = customerSlice.actions
 
-export function updateName(fullName) {
-    return { type: 'customer/updateName', payload: { fullName } }
-}
+export default customerSlice.reducer
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+//     switch (action.type) {
+//         case 'customer/createCustomer':
+//             return {
+//                 ...state,
+//                 fullName: action.payload.fullName,
+//                 nationalId: action.payload.nationalId,
+//                 createdAt: action.payload.createdAt,
+//             }
+//         case 'customer/updateName':
+//             return { ...state, fullName: action.payload }
+//         default:
+//             return state
+//     }
+// }
+
+// export function createCustomer(fullName, nationalId) {
+//     return { type: 'customer/createCustomer', payload: { fullName, nationalId, createdAt: new Date().toISOString() } }
+// }
+
+// export function updateName(fullName) {
+//     return { type: 'customer/updateName', payload: { fullName } }
+// }
