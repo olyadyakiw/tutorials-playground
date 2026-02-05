83011f5a9a763af58e2059cfa3939f6d946087fb
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-05 21:45:34 +0100

    continue ultimate-react-course/17-the-wild-oasis (react-query)

--- ultimate-react-course-main/17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ ultimate-react-course-main/17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -1,89 +1,116 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import Input from "../../ui/Input";
-import Form from "../../ui/Form";
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Textarea from "../../ui/Textarea";
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import Input from '../../ui/Input'
+import FormRow from '../../ui/FormRow'
+import Form from '../../ui/Form'
+import Button from '../../ui/Button'
+import FileInput from '../../ui/FileInput'
+import Textarea from '../../ui/Textarea'
+import { useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createCabin } from '../../services/apiCabins'
+import toast from 'react-hot-toast'
 
 function CreateCabinForm() {
-  return (
-    <Form>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
-      </FormRow>
+    const queryClient = useQueryClient()
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
-      </FormRow>
+    const { register, handleSubmit, reset, getValues, formState } = useForm()
+    const { errors } = formState
+    console.log(errors)
+    const { mutate, isLoading: isCreating } = useMutation({
+        mutationFn: createCabin,
+        onSuccess: () => {
+            toast.success('Added!')
+            queryClient.invalidateQueries({ queryKey: ['cabins'] })
+            reset()
+        },
+        onError: err => toast.error(err.message),
+    })
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
-      </FormRow>
+    function onSubmit(data) {
+        mutate(data)
+    }
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
-      </FormRow>
+    function onError(errors) {
+        console.log(errors)
+    }
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
-      </FormRow>
+    return (
+        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+            <FormRow label="Cabin name" error={errors?.name?.message}>
+                <Input
+                    disabled={isCreating}
+                    type="text"
+                    id="name"
+                    {...register('name', { required: 'This field is required' })}
+                />
+            </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
+            <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+                <Input
+                    disabled={isCreating}
+                    type="number"
+                    id="maxCapacity"
+                    {...register('maxCapacity', {
+                        required: 'This field is required',
+                        min: {
+                            value: 1,
+                            message: 'Capacity should at least 1',
+                        },
+                    })}
+                />
+            </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Edit cabin</Button>
-      </FormRow>
-    </Form>
-  );
+            <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+                <Input
+                    disabled={isCreating}
+                    type="number"
+                    id="regularPrice"
+                    {...register('regularPrice', {
+                        required: 'This field is required',
+                        min: {
+                            value: 1,
+                            message: 'Capacity should at least 1',
+                        },
+                    })}
+                />
+            </FormRow>
+
+            <FormRow label="Discount" error={errors?.discount?.message}>
+                <Input
+                    disabled={isCreating}
+                    type="number"
+                    id="discount"
+                    defaultValue={0}
+                    {...register('discount', {
+                        required: 'This field is required',
+                        validate: value => value <= getValues().regularPrice || 'Discount should be less than price',
+                    })}
+                />
+            </FormRow>
+
+            <FormRow label="Description" error={errors?.description?.message}>
+                <Textarea
+                    disabled={isCreating}
+                    type="number"
+                    id="description"
+                    defaultValue=""
+                    {...register('description', { required: 'This field is required' })}
+                />
+            </FormRow>
+
+            <FormRow label="Cabin photo">
+                <FileInput id="image" accept="image/*" />
+            </FormRow>
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button variation="secondary" type="reset">
+                    Cancel
+                </Button>
+                <Button disabled={isCreating}>Edit cabin</Button>
+            </FormRow>
+        </Form>
+    )
 }
 
-export default CreateCabinForm;
+export default CreateCabinForm
