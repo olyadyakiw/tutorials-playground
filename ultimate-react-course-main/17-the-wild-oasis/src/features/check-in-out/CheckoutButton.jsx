60e1defac7ae7ce5c1fec7b484912219d997e6b4
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-17 17:09:18 +0100

    finish ultimate-react-course/17-the-wild-oasis

--- ultimate-react-course-main/17-the-wild-oasis/src/features/check-in-out/CheckoutButton.jsx
+++ ultimate-react-course-main/17-the-wild-oasis/src/features/check-in-out/CheckoutButton.jsx
@@ -1,11 +1,14 @@
-import Button from "../../ui/Button";
+import Button from '../../ui/Button'
+import { useCheckOut } from './useCheckOut'
 
 function CheckoutButton({ bookingId }) {
-  return (
-    <Button variation="primary" size="small">
-      Check out
-    </Button>
-  );
+    const { checkout, isCheckingOut } = useCheckOut()
+
+    return (
+        <Button variation="primary" size="small" onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
+            Check out
+        </Button>
+    )
 }
 
-export default CheckoutButton;
+export default CheckoutButton
--- ultimate-react-course-main/17-the-wild-oasis/src/features/dashboard/SalesChart.jsx
+++ ultimate-react-course-main/17-the-wild-oasis/src/features/dashboard/SalesChart.jsx
@@ -83,7 +83,9 @@ function SalesChart({ bookings, numDays }) {
 
     return (
         <StyledSalesChart>
-            <Heading as="h2">Sales</Heading>
+            <Heading as="h2">
+                Sales from {format(allDates.at(0), 'MMM dd yyyy')} to {format(allDates.at(-1), 'MMM dd yyyy')}
+            </Heading>
 
             <ResponsiveContainer height={300} width="100%">
                 <AreaChart data={data}>
