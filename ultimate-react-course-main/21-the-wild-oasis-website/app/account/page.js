0948fb7584727034abfb8d82a36bc05642f5b193
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-19 20:57:00 +0100

    continue ultimate-react-course/21-the-wild-oasis-website

--- ultimate-react-course-main/21-the-wild-oasis-website/app/account/page.js
+++ ultimate-react-course-main/21-the-wild-oasis-website/app/account/page.js
@@ -1,7 +1,7 @@
+export const metadata = {
+    title: 'Guest area',
+}
+
 export default function Page() {
-    return (
-        <div>
-            <h1>Your account</h1>
-        </div>
-    )
+    return <h2 className="font-semibold text-2xl text-accent-400 mb-7">`Welcome. Olha</h2>
 }
--- ultimate-react-course-main/21-the-wild-oasis-website/app/layout.js
+++ ultimate-react-course-main/21-the-wild-oasis-website/app/layout.js
@@ -1,20 +1,37 @@
-import Logo from './components/Logo'
-import Navigation from './components/Navigation'
+import Logo from '@/app/_components/Logo'
+import Navigation from '@/app/_components/Navigation'
+
+import { Josefin_Sans } from 'next/font/google'
+
+const josefin = Josefin_Sans({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+import '@/app/_styles/globals.css'
+import Header from './_components/Header'
 
 export const metadata = {
-    title: 'The Wild Oasis',
+    // title: 'The Wild Oasis',
+    title: {
+        template: '%s / The Wild Oasis',
+        default: 'Welcome / The wild Oasis',
+    },
+    description:
+        'Luxurious cavin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests',
 }
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
-                <header>
-                    <Logo />
-                </header>
-                <Navigation />
-                <main>{children}</main>
-                <footer>Copyright by The Wild Oasis</footer>
+            <body
+                className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+            >
+                <Header />
+
+                <div className="flex-1 px-8 py-12 grid">
+                    <main className="max-w-7xl mx-auto w-full">{children}</main>
+                </div>
             </body>
         </html>
     )
