864dc1fce492c711db5bcac9efb5784a9cb22ffb
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-21 18:04:22 +0100

    continue ultimate-react-course/21-the-wild-oasis-website

--- ultimate-react-course-main/21-the-wild-oasis-website/app/_lib/data-service.js
+++ ultimate-react-course-main/21-the-wild-oasis-website/app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from 'date-fns'
 import { supabase } from './supabase'
+import { notFound } from 'next/navigation'
 
 /////////////
 // GET
@@ -12,6 +13,11 @@ export async function getCabin(id) {
 
     if (error) {
         console.error(error)
+    }
+
+    if (error) {
+        console.error(error)
+        notFound()
     }
 
     return data
