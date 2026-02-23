8d33025aa70b843641f77cb56e6bc88110d50b67
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-23 12:58:28 +0100

    continue ultimate-react-course/21-the-wild-oasis-website

---
+++ ultimate-react-course-main/21-the-wild-oasis-website/app/_components/Reservation.js
@@ -0,0 +1,16 @@
+import DateSelector from '@/app/_components/DateSelector'
+import ReservationForm from '@/app/_components/ReservationForm'
+import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service'
+
+async function Reservation({ cabin }) {
+    const [settings, bookDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)])
+
+    return (
+        <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+            <DateSelector settings={settings} bookDates={bookDates} cabin={cabin} />
+            <ReservationForm cabin={cabin} />
+        </div>
+    )
+}
+
+export default Reservation
