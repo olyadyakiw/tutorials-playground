caf04b1df6f95fd8af3618a53a589ae3de7c3a98
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-24 19:27:40 +0100

    continue ultimate-react-course/21-the-wild-oasis-website

--- ultimate-react-course-main/21-the-wild-oasis-website/app/_components/Reservation.js
+++ ultimate-react-course-main/21-the-wild-oasis-website/app/_components/Reservation.js
@@ -1,14 +1,17 @@
 import DateSelector from '@/app/_components/DateSelector'
 import ReservationForm from '@/app/_components/ReservationForm'
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service'
+import { auth } from '../_lib/auth'
+import LoginMessage from './LoginMessage'
 
 async function Reservation({ cabin }) {
     const [settings, bookDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)])
+    const session = await auth()
 
     return (
         <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
             <DateSelector settings={settings} bookDates={bookDates} cabin={cabin} />
-            <ReservationForm cabin={cabin} />
+            {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
         </div>
     )
 }
