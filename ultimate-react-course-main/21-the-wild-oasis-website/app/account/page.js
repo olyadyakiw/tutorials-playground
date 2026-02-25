1a47905fa2824f7f6ea4539fd82e8762f9a8e0be
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-25 08:32:45 +0100

    continue ultimate-react-course/21-the-wild-oasis-website

--- ultimate-react-course-main/21-the-wild-oasis-website/app/account/page.js
+++ ultimate-react-course-main/21-the-wild-oasis-website/app/account/page.js
@@ -1,7 +1,11 @@
+import { auth } from '../_lib/auth'
+
 export const metadata = {
     title: 'Guest area',
 }
 
-export default function Page() {
-    return <h2 className="font-semibold text-2xl text-accent-400 mb-7">`Welcome. Olha</h2>
+export default async function Page() {
+    const sesstion = await auth()
+    const firstName = sesstion.user.name.split(' ')[0]
+    return <h2 className="font-semibold text-2xl text-accent-400 mb-7">`Welcome, {firstName}</h2>
 }
