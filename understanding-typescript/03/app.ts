fb9deb28ed43db27de2d202dc090e6bd0c7240d0
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-02-27 17:49:46 +0100

    add understanding-typescript/03

---
+++ understanding-typescript/03/app.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs'
+
+// fs.readFileSync()
+
+let userName: string
+userName = 'Olha'
+console.log(userName)
+
+function add(a: any, b: any) {
+    return a + b
+}
+
+add(10, 5)
