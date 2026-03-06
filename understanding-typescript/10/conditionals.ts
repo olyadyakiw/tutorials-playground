5e43b0e391a89c03c63efc750056395b973b6ae0
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-03-06 20:39:16 +0100

    add understanding-typescript/10

---
+++ understanding-typescript/10/conditionals.ts
@@ -0,0 +1,26 @@
+type StringArray = string[]
+// type ElementType<T extends any[]> = T[number]
+
+// type Example1 = ElementType<StringArray>
+
+let text = ' hello'
+
+// type Example2 = ElementType<typeof text>
+
+type GetElementType<T> = T extends any[] ? T[number] : never
+type Example1 = GetElementType<StringArray>
+type Example2 = GetElementType<typeof text>
+
+type FullnamePerson = { firstName: string; lastName: string }
+type FullnameOrNothing<T> = T extends FullnamePerson ? string : never
+
+function getFullName<T extends object>(person: T): FullnameOrNothing<T> {
+    if ('firstName' in person && 'lastName' in person && person.firstName && person.lastName) {
+        return `${person.firstName} ${person.lastName}` as FullnameOrNothing<T>
+    }
+
+    throw new Error('No first name and / or last name found')
+}
+
+const name1 = getFullName({})
+const name2 = getFullName({ firstName: '1', lastName: '2' })
---
+++ understanding-typescript/10/keyof.ts
@@ -0,0 +1,22 @@
+type User = { name: string; age: number }
+type UserKeys = keyof User
+
+let validKey: UserKeys
+
+validKey = 'name'
+validKey = 'age'
+
+function getProp<T extends object, U extends keyof T>(obj: T, key: U) {
+    const val = obj[key]
+    if (val === undefined || val === null) {
+        throw new Error('Accessing undedined or null value')
+    }
+
+    return val
+}
+
+const data = { id: 1, isStored: false, values: [1, -5, 10] }
+
+const user = { name: 'Max', age: 35 }
+
+const val = getProp(user, 'age')
