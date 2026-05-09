a93e5f2ef724dd861381e09cb1f466c8acdb27fc
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-05-09 13:01:29 +0200

    finish coding train-challanges/52 Random walker

---
+++ coding-train-challenges/052/sketch.js
@@ -0,0 +1,32 @@
+let x
+let y
+
+function setup() {
+    createCanvas(400, 400)
+    x = 200
+    y = 200
+    background(51)
+}
+
+function draw() {
+    stroke(255)
+    strokeWeight(2)
+    point(x, y)
+
+    let r = floor(random(4))
+
+    switch (r) {
+        case 0:
+            x = x + 1
+            break
+        case 1:
+            x = x - 1
+            break
+        case 2:
+            y = y + 1
+            break
+        case 3:
+            y = y - 1
+            break
+    }
+}
