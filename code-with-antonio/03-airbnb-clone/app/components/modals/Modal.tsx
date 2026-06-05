3f8448d927a9e25e38eeebd8b96661355816f2c7
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-06-05 18:51:24 +0200

    continue code-with-antonio/03-airbnb-clone register and login functionality

--- code-with-antonio/03-airbnb-clone/app/components/modals/Modal.tsx
+++ code-with-antonio/03-airbnb-clone/app/components/modals/Modal.tsx
@@ -91,6 +91,7 @@ const Modal: React.FC<ModalProps> = ({
                                     )}
                                     <Button disabled={disabled} label={actionLabel} onClick={handleSubmit} />
                                 </div>
+                                {footer}
                             </div>
                         </div>
                     </div>
--- code-with-antonio/03-airbnb-clone/app/layout.tsx
+++ code-with-antonio/03-airbnb-clone/app/layout.tsx
@@ -3,7 +3,10 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal.'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
     title: 'Airbnb',
@@ -14,13 +17,17 @@ const font = Nunito({
     subsets: ['latin'],
 })
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+    const currentUser = await getCurrentUser()
+
     return (
         <html lang="en">
             <body className={font.className}>
                 <ClientOnly>
-                    <Modal isOpen title="Hello world" />
-                    <Navbar />
+                    <ToasterProvider />
+                    <RegisterModal />
+                    <LoginModal />
+                    <Navbar currentUser={currentUser} />
                 </ClientOnly>
                 {children}
             </body>
