62a739b07c15ad0d04b0b569ba181dc071d169c9
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-06-15 18:34:28 +0200

    finish code-with-antinio 05-spotify-clone Stripe integration

--- code-with-antonio/05-spotify-clone/components/Library.tsx
+++ code-with-antonio/05-spotify-clone/components/Library.tsx
@@ -9,6 +9,7 @@ import { Song } from '@/types'
 import SongItem from './SongItem'
 import MediaItem from './MediaItem'
 import useOnPlay from '@/hooks/useOnPlay'
+import useSubscribeModal from '@/hooks/useSubscribeModal'
 
 interface LibraryProps {
     songs: Song[]
@@ -17,13 +18,18 @@ interface LibraryProps {
 const Library: React.FC<LibraryProps> = ({ songs }) => {
     const authModal = useAuthModal()
     const uploadModal = useUploadModal()
-    const { user } = useUser()
+    const subscribeModal = useSubscribeModal()
+    const { user, subscription } = useUser()
 
     const onPlay = useOnPlay(songs)
 
     const onClick = () => {
         if (!user) {
             return authModal.onOpen()
+        }
+
+        if (!subscription) {
+            return subscribeModal.onOpen()
         }
 
         return uploadModal.onOpen()
