a0d5925de39218cfe81b45b77a55a377df9ddf57
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-06-15 14:06:13 +0200

    continue code-with-antinio 05-spotify-clone Player functionality

--- code-with-antonio/05-spotify-clone/app/liked/components/LikedContent.tsx
+++ code-with-antonio/05-spotify-clone/app/liked/components/LikedContent.tsx
@@ -3,23 +3,34 @@
 import LikedButton from '@/components/LikedButton'
 import MediaItem from '@/components/MediaItem'
 import useOnPlay from '@/hooks/useOnPlay'
+import { useUser } from '@/hooks/useUser'
 import { Song } from '@/types'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-interface SearchContentProps {
+interface LikedContentProps {
     songs: Song[]
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+    const router = useRouter()
+    const { isLoading, user } = useUser()
     const onPlay = useOnPlay(songs)
 
+    useEffect(() => {
+        if (!isLoading && !user) {
+            router.replace('/')
+        }
+    }, [isLoading, user, router])
+
     if (songs.length === 0) {
-        return <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">No songs found</div>
+        return <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">No liked songs.</div>
     }
 
     return (
-        <div className="flex flex-col gap-y-2 w-full px-6">
+        <div className="flex flex-col gap-y-2 w-full p-6">
             {songs.map(song => (
-                <div key={song.id} className="flex items-center gap-x-4 w-full">
+                <div className="flex items-center gap-x-4 w-full" key={song.id}>
                     <div className="flex-1">
                         <MediaItem
                             onClick={(id: string) => {
@@ -35,4 +46,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
     )
 }
 
-export default SearchContent
+export default LikedContent
--- code-with-antonio/05-spotify-clone/components/Library.tsx
+++ code-with-antonio/05-spotify-clone/components/Library.tsx
@@ -8,6 +8,7 @@ import useUploadModal from '@/hooks/useUploadModal'
 import { Song } from '@/types'
 import SongItem from './SongItem'
 import MediaItem from './MediaItem'
+import useOnPlay from '@/hooks/useOnPlay'
 
 interface LibraryProps {
     songs: Song[]
@@ -17,6 +18,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
     const authModal = useAuthModal()
     const uploadModal = useUploadModal()
     const { user } = useUser()
+
+    const onPlay = useOnPlay(songs)
 
     const onClick = () => {
         if (!user) {
@@ -41,7 +44,15 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
                 {songs.map(item => {
-                    return <MediaItem onClick={() => {}} key={item.id} data={item} />
+                    return (
+                        <MediaItem
+                            onClick={(id: string) => {
+                                onPlay(id)
+                            }}
+                            key={item.id}
+                            data={item}
+                        />
+                    )
                 })}
             </div>
         </div>
