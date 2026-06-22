825eab452bf17f534259e5b45f11755941c9c5ff
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-06-22 10:28:52 +0200

    continue code-with-antinio 06-e-commerce clone setup store, add featured products

---
+++ code-with-antonio/06-e-commerce/store/app/layout.tsx
@@ -0,0 +1,23 @@
+import Footer from '@/components/footer'
+import './globals.css'
+import { Urbanist } from 'next/font/google'
+import Navbar from '@/components/navbar'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata = {
+    title: 'Store',
+    description: 'Store',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    )
+}
---
+++ code-with-antonio/06-e-commerce/store/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">&copy; 2026 FakeStore, Inc. All rights reserved.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
