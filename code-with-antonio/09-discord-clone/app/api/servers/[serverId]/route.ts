893ca70de7877be3564939aa3e27e77a4e6f2fdc
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-03 14:02:08 +0200

    continue code-with-antonio/09-discord-clone add modals to sidebar

--- code-with-antonio/09-discord-clone/app/api/servers/[serverId]/route.ts
+++ code-with-antonio/09-discord-clone/app/api/servers/[serverId]/route.ts
@@ -29,3 +29,25 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
         return new NextResponse('internal error', { status: 500 })
     }
 }
+
+export async function DELETE(req: Request, { params }: { params: { serverId: string } }) {
+    try {
+        const profile = await currentProfile()
+
+        if (!profile) {
+            return new NextResponse('Unauth', { status: 401 })
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            },
+        })
+
+        return NextResponse.json(server)
+    } catch (error) {
+        console.log('[Server_id_delete]', error)
+        return new NextResponse('internal error', { status: 500 })
+    }
+}
