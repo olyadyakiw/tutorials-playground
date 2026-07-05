db08c85fb831a96cb7266fdedbff9660320c44e9
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-05 17:29:32 +0200

    finish code-with-antonio/09-discord-clone

--- code-with-antonio/09-discord-clone/next.config.js
+++ code-with-antonio/09-discord-clone/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: config => {
+        config.externals.push({
+            'utf-8-validate': 'commonjs utf-8-validate',
+            bufferutil: 'commonjs',
+        })
+
+        return config
+    },
     images: {
         domains: ['y47b5s59g1.ufs.sh', 'utfs.io'],
     },
