907b98af69b3ece2eafe2a4f55f4074a24ff960c
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-09 19:59:47 +0200

    continue code-with-antonio/10-udemy-clone add chapter edit page

--- code-with-antonio/10-udemy-clone/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ code-with-antonio/10-udemy-clone/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { ImageForm } from './_components/image-form'
 import { CategoryForm } from './_components/category-form'
 import { PriceForm } from './_components/price-form'
 import { AttachmentForm } from './_components/attachment-form'
+import { ChaptersForm } from './_components/chapters-form'
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth()
@@ -20,8 +21,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const course = await db.course.findUnique({
         where: {
             id: params.courseId,
+            userId,
         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: 'asc',
+                },
+            },
             attachments: {
                 orderBy: {
                     createdAt: 'desc',
@@ -40,7 +47,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
         return redirect('/')
     }
 
-    const requiredFields = [course.title, course.description, course.imageUrl, course.price, course.categoryId]
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
+    ]
 
     const totalFields = requiredFields.length
     const completedFields = requiredFields.filter(Boolean).length
@@ -75,11 +89,11 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                 </div>
                 <div className="space-y-6">
                     <div>
-                        <div className="flex items-center gap-x-x2">
+                        <div className="flex items-center gap-x-2">
                             <IconBadge icon={ListChecks} />
                             <h2 className="text-xl">Course chapters</h2>
                         </div>
-                        <div>TODO Chapters</div>
+                        <ChaptersForm initialData={course} courseId={course.id} />
                     </div>
                     <div>
                         <div className="flex items-center gap-x-2">
