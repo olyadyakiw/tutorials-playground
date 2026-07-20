55a38359182b3d83817943615ea57458a40b487d
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-20 09:54:02 +0200

    continue code-with-antonio/11-notion-clone add banner and toolbar

--- code-with-antonio/11-notion-clone/app/(main)/_components/navbar.tsx
+++ code-with-antonio/11-notion-clone/app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { useQuery } from 'convex/react'
 import { MenuIcon } from 'lucide-react'
 import { useParams } from 'next/navigation'
 import Banner from './banner'
+import Menu from './menu'
 
 interface NavbarProps {
     isCollapsed: boolean
@@ -20,8 +21,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
     if (document === undefined) {
         return (
-            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between">
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -38,6 +42,9 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
             {document.isArchived && <Banner documentId={document._id} />}
