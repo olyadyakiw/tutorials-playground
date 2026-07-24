e7e87deba39fbd6998603c5f119d6f97ec21fe56
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-24 23:00:50 +0200

    continue code-with-antonio/12-trello-clone add useAction abstraction

--- code-with-antonio/12-trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ code-with-antonio/12-trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,3 @@
-import { create } from '@/actions/create-board'
 import { Button } from '@/components/ui/button'
 import { db } from '@/lib/db'
 
@@ -6,7 +5,7 @@ const OrganizationIdPage = async () => {
     const boards = await db.board.findMany()
     return (
         <div className="flex flex-col space-y-4">
-            <form action={create}>
+            <form>
                 <input
                     id="title"
                     name="title"
