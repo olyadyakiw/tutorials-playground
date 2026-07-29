0d63a11ebabac4f3bef861785012ce0f698ef1f6
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-29 13:17:42 +0200

    continue code-with-antonio/12-trello-clone add Drag n' Drop

--- code-with-antonio/12-trello-clone/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ code-with-antonio/12-trello-clone/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Draggable } from '@hello-pangea/dnd'
 import { Card } from '@prisma/client'
 
 interface CardItemProps {
@@ -9,12 +10,19 @@ interface CardItemProps {
 
 const CardItem = ({ data, index }: CardItemProps) => {
     return (
-        <div
-            role="button"
-            className="truncate border-2 border-transparent hover:border-black py-3 px-3 text-sm bg-white rounded-md shadow-sm"
-        >
-            {data.title}
-        </div>
+        <Draggable draggableId={data.id} index={index}>
+            {provided => (
+                <div
+                    {...provided.dragHandleProps}
+                    {...provided.draggableProps}
+                    ref={provided.innerRef}
+                    role="button"
+                    className="truncate border-2 border-transparent hover:border-black py-3 px-3 text-sm bg-white rounded-md shadow-sm"
+                >
+                    {data.title}
+                </div>
+            )}
+        </Draggable>
     )
 }
 
