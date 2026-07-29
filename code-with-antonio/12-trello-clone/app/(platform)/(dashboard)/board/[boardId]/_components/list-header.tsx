3915dfee9210a6afbb8a9a0ee3de2423031d9d5c
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-07-29 12:22:51 +0200

    continue code-with-antonio/12-trello-clone add Card Form

--- code-with-antonio/12-trello-clone/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ code-with-antonio/12-trello-clone/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -12,9 +12,10 @@ import ListOptions from './list-options'
 
 interface ListHeaderProps {
     data: List
+    onAddCard: () => void
 }
 
-const ListHeader = ({ data }: ListHeaderProps) => {
+const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
     const [title, setTitle] = useState(data.title)
     const [isEditing, setIsEditing] = useState(false)
 
@@ -93,7 +94,7 @@ const ListHeader = ({ data }: ListHeaderProps) => {
                     {title}
                 </div>
             )}
-            <ListOptions data={data} onAddCard={() => {}} />
+            <ListOptions data={data} onAddCard={onAddCard} />
         </div>
     )
 }
