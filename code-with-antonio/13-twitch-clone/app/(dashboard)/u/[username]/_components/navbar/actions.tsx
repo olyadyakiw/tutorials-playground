39aa9e948e29a570c1c4940a852034473603a76a
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-08-04 19:18:05 +0200

    continue code-with-antonio/13-twitch-clone add Creator dashboard

---
+++ code-with-antonio/13-twitch-clone/app/(dashboard)/u/[username]/_components/navbar/actions.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { currentUser, SignInButton, UserButton } from '@clerk/nextjs'
+import { Clapperboard, LogOut } from 'lucide-react'
+import Link from 'next/link'
+
+const Actions = async () => {
+    return (
+        <div className="flex items-center justify-end gap-x-2">
+            <Button size="sm" variant="ghost" className="text-muted-foreground hover:text-primary" asChild>
+                <Link href="/">
+                    <LogOut className="h-5 w-5 mr-2" />
+                    <span className="hidden lg:block">Exit</span>
+                </Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    )
+}
+
+export default Actions
