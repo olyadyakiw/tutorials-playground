bbff47737951e6a9fc401845e34c830038cc9308
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-08-05 14:20:39 +0200

    continue code-with-antonio/13-twitch-clone add chat settings

--- code-with-antonio/13-twitch-clone/app/(dashboard)/u/[username]/_components/sidebar/index.tsx
+++ code-with-antonio/13-twitch-clone/app/(dashboard)/u/[username]/_components/sidebar/index.tsx
@@ -1,7 +1,14 @@
+import Navigation from './navigation'
+import Toggle from './toggle'
 import { Wrapper } from './wrapper'
 
 const Sidebar = () => {
-    return <Wrapper>Sidebar</Wrapper>
+    return (
+        <Wrapper>
+            <Toggle />
+            <Navigation />
+        </Wrapper>
+    )
 }
 
 export default Sidebar
--- code-with-antonio/13-twitch-clone/app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
+++ code-with-antonio/13-twitch-clone/app/(dashboard)/u/[username]/_components/sidebar/wrapper.tsx
@@ -8,10 +8,13 @@ interface WrapperProps {
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+    const { collapsed } = useCreatorSidebar(state => state)
+
     return (
         <aside
             className={cn(
                 'fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50',
+                collapsed && 'lg:w-[70px]',
             )}
         >
             {children}
