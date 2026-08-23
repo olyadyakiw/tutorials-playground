be25ffaaffcf47ff3d267abe8d80731f25820cc8
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-08-23 16:32:45 +0200

    continue code-with-antonio/15-memo-clone add Selection Box

--- code-with-antonio/15-miro-clone/app/board/[boardId]/_components/canvas.tsx
+++ code-with-antonio/15-miro-clone/app/board/[boardId]/_components/canvas.tsx
@@ -11,6 +11,7 @@ import { connectionIdToColor, pointerEventToCanvasPoint } from '@/lib/utils'
 import { nanoid } from 'nanoid'
 import { LiveObject } from '@liveblocks/client'
 import LayerPreview from './layer-preview'
+import SelectionBox from './selection-box'
 
 const MAX_LAYERS = 100
 
@@ -165,6 +166,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
                             selectionColor={layerIdsToColorSelection[layerId]}
                         />
                     ))}
+                    <SelectionBox onResizeHandlePointerDown={() => {}} />
                     <CursorsPresence />
                 </g>
             </svg>
