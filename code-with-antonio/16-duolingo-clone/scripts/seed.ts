aaabbc4789853ccdb2b9f8d53e97d83c31c0811b
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-09-04 12:03:42 +0200

    continue code-with-antonio/16-duolingo-clone add Lesson Button

--- code-with-antonio/16-duolingo-clone/scripts/seed.ts
+++ code-with-antonio/16-duolingo-clone/scripts/seed.ts
@@ -60,6 +60,30 @@ const main = async () => {
                 title: 'Nouns',
                 order: 1,
             },
+            {
+                id: 2,
+                unitId: 1,
+                title: 'Verbs',
+                order: 2,
+            },
+            {
+                id: 3,
+                unitId: 1,
+                title: 'Nouns',
+                order: 3,
+            },
+            {
+                id: 4,
+                unitId: 1,
+                title: 'Nouns',
+                order: 4,
+            },
+            {
+                id: 5,
+                unitId: 1,
+                title: 'Nouns',
+                order: 5,
+            },
         ])
 
         await db.insert(schema.challenges).values([
