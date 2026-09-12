d461846b13a128cd8c960ae3404eea9e1ecf9ee0
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-09-12 21:57:09 +0200

    continue code-with-antonio/16-duolingo-clone add Challenge Footer

--- code-with-antonio/16-duolingo-clone/app/lesson/card.tsx
+++ code-with-antonio/16-duolingo-clone/app/lesson/card.tsx
@@ -1,6 +1,8 @@
 import { challenges } from '@/db/schema'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import { useCallback } from 'react'
+import { useAudio, useKey } from 'react-use'
 
 type Props = {
     id: number
@@ -16,9 +18,20 @@ type Props = {
 }
 
 const Card = ({ id, imageSrc, audioSrc, text, shortcut, selected, onClick, disabled, status, type }: Props) => {
+    const [audio, _, controls] = useAudio({ src: audioSrc || '' })
+
+    const handleClick = useCallback(() => {
+        if (disabled) return
+
+        controls.play()
+        onClick()
+    }, [disabled, onClick, controls])
+
+    useKey(shortcut, handleClick, {}, [handleClick])
+
     return (
         <div
-            onClick={() => {}}
+            onClick={handleClick}
             className={cn(
                 'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2',
                 selected && 'border-sky-300 bg-sky-100 hover:bg-sky-100',
@@ -28,6 +41,7 @@ const Card = ({ id, imageSrc, audioSrc, text, shortcut, selected, onClick, disab
                 type === 'ASSIST' && 'lg:p-3 w-full',
             )}
         >
+            {audio}
             {imageSrc && (
                 <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
                     <Image src={imageSrc} fill alt={text} />
